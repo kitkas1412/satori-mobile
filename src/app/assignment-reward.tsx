cf48ce06dbd64c6005f85ca8b87b3f78ad950549
dinhduc <ducndhe171600@fpt.edu.vn>
2026-04-15 17:48:57 +0700

    refactor(assignment): remove assignment-reward screen and related logic for cleaner flow

--- src/app/assignment-reward.tsx
+++
@@ -1,5 +0,0 @@
-import { AssignmentRewardScreen } from "@/features/assignment/screens/assignment-reward-screen";
-
-export default function AssignmentReward() {
-  return <AssignmentRewardScreen />;
-}
